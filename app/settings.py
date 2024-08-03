c38c0d2eb66f544aebba2f6cf4d0533586a09543
Haozilla <wugleglewu@gmail.com>
2024-08-03 17:10:10 +0800

    refactor: update database URI handling in settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,19 +1,11 @@
 import os
-import sys
 
 from app import app
 
-# SQLite URI compatible
-WINDOWS = sys.platform.startswith('win')
-if WINDOWS:
-    prefix = 'sqlite:///'
-else:
-    prefix = 'sqlite:////'
 
-
-# /data.py
-dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+prefix = 'sqlite:////'
+database = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', database)
